796923e7878a4e105b3c86f1a1197b496ac579a2
MackenzieZRS <Mackenziezrs@gmail.com>
2026-05-01 18:55:03 -0400

    Fix missing useEffect import in Step2Dates

--- components/quiz/steps/Step2Dates.tsx
+++ components/quiz/steps/Step2Dates.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { QuizState } from "@/lib/types";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
